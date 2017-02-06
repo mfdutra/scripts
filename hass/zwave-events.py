5f4282d5f8dca1cb771cbcfc4b8dd6cb193269fc
Marlon Dutra <mfdutra@gmail.com>
2017-02-05 22:03:08 -0800

    Introduce cloning of scene controllers

--- hass/zwave-events.py
+++ hass/zwave-events.py
@@ -8,6 +8,11 @@ import re
 uri = 'http://127.0.0.1:8123/api/events/custom_scene'
 apiKey = '***'
 
+# Use replaceMap to clone scene controllers
+replaceMap = {
+  'Node007': 'Node002',
+}
+
 eventRe = re.compile(r'Info, (Node\d+), Received Central Scene.*scene id=(\d+) in (\d+) seconds\.')
 
 log = Popen(('/usr/bin/tail', '-F', '-n', '0', '/home/hass/hass/OZW_Log.txt'), stdout=PIPE)
@@ -23,5 +28,8 @@ while True:
 
   match = eventRe.search(line)
   if match:
-    data = {'button': '_'.join(match.groups())}
+    groups = list(match.groups())
+    if groups[0] in replaceMap:
+      groups[0] = replaceMap[groups[0]]
+    data = {'button': '_'.join(groups)}
     post(uri, data=json.dumps(data), headers={'x-ha-access': apiKey, 'content-type': 'application/json'})
