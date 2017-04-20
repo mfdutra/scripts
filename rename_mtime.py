e245674f8b257b8be17c631b146a5be2754c926f
Marlon Dutra <mfdutra@fb.com>
2017-04-19 21:51:45 -0700

    add rename_mtime.py

---
+++ rename_mtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.6
+
+import datetime
+import os
+import sys
+
+for f in sys.argv[1:]:
+    mtime = datetime.datetime.fromtimestamp(os.stat(f)[8])
+    if '.' in f:
+        ext = '.' + f.rpartition('.')[2]
+    else:
+        ext = ''
+    new = mtime.strftime('%Y%m%d-%H%M%S') + ext
+
+    if os.path.exists(new):
+        print('{} already exists'.format(new), file=sys.stderr)
+    else:
+        print('{} -> {}'.format(f, new))
+        os.rename(f, new)
