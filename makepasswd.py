5e48cd79769a16efbdf5328007f5bddbe78cc3dc
Marlon Dutra <mfdutra@fb.com>
2018-01-10 14:36:00 -0800

    16 chars for password

--- makepasswd.py
+++ makepasswd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Generate a random password with 8 chars
+# Generate a random password
 
 # Copyright 2011 Marlon Dutra
 #
@@ -19,7 +19,7 @@
 
 import random
 
-CHARS=8
+CHARS=16
 
 data = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9')
 
