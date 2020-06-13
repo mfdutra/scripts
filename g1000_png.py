37b107c43eb91787d252202c5eb57d3710ab2e84
Marlon Dutra <mfdutra@fb.com>
2020-06-12 18:56:17 -0700

    add egt mode to g1000_png.py

--- g1000_png.py
+++ g1000_png.py
@@ -145,13 +145,63 @@ FLT TIME: {data['X-FlightTime']}
 '''
 
     img = Image.new('RGB', (args.width, args.height))
-
     fnt = ImageFont.truetype('/Library/Fonts/Courier New Bold.ttf', 14)
     d = ImageDraw.Draw(img)
     d.text((10, 10), text, fill=(0, 255, 0), font=fnt)
 
     img.save(args.outdir + f'/{data["i"]:06d}.png')
 
+    log(data['Lcl Date'] + ' ' + data['Lcl Time'])
+
+def processEGT(data):
+    img = Image.new('RGB', (args.width, args.height))
+    fnt = ImageFont.truetype('/Library/Fonts/Courier New Bold.ttf', 14)
+    d = ImageDraw.Draw(img)
+
+    egts = []
+    egts.append(intOrZero(data['E1 EGT1']))
+    egts.append(intOrZero(data['E1 EGT2']))
+    egts.append(intOrZero(data['E1 EGT3']))
+    egts.append(intOrZero(data['E1 EGT4']))
+
+    chts = []
+    chts.append(intOrZero(data['E1 CHT1']))
+    chts.append(intOrZero(data['E1 CHT2']))
+    chts.append(intOrZero(data['E1 CHT3']))
+    chts.append(intOrZero(data['E1 CHT4']))
+
+    x, y = 1700, 800
+    for egt in egts:
+        bar = max(0, egt - 1100) // 2
+        d.rectangle(((x, y), (x + 20, y - bar)), fill=(0, 255, 0))
+        d.text((x, y + 10), str(egt), fill=(0, 255, 0), font=fnt)
+
+        x += 40
+
+    x, y = 1700, 1000
+    for cht in chts:
+        bar = max(0, cht - 200) // 2
+        d.rectangle(((x, y), (x + 20, y - bar)), fill=(0, 255, 0))
+        d.text((x, y + 10), str(cht), fill=(0, 255, 0), font=fnt)
+
+        x += 40
+
+    ff = floatOrZero(data['E1 FFlow'])
+    mp = floatOrZero(data['E1 MAP'])
+
+    d.rectangle(((1700, 500), (1720, 500 - (ff * 6))), fill=(0, 255, 0))
+    d.text((1700, 510), str(ff), fill=(0, 255, 0), font=fnt)
+
+    d.rectangle(((1800, 500), (1820, 500 - (mp * 4))), fill=(0, 255, 0))
+    d.text((1800, 510), str(mp), fill=(0, 255, 0), font=fnt)
+
+    # Labels
+    d.text((1765, 480), 'MAP', fill=(0, 255, 0), font=fnt)
+    d.text((1665, 480), ' FF', fill=(0, 255, 0), font=fnt)
+    d.text((1665, 780), 'EGT', fill=(0, 255, 0), font=fnt)
+    d.text((1665, 980), 'CHT', fill=(0, 255, 0), font=fnt)
+
+    img.save(args.outdir + f'/{data["i"]:06d}.png')
     log(data['Lcl Date'] + ' ' + data['Lcl Time'])
 
 def log(msg):
@@ -193,6 +243,13 @@ def intOrZero(n):
     except ValueError:
         return 0
 
+def floatOrZero(n):
+    try:
+        return float(n)
+    except ValueError:
+        return 0
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Create PNG sequence out of Garmin G1000 data log'
@@ -204,6 +261,8 @@ def main():
         help='Image width [%(default)s]')
     parser.add_argument('--height', default=1080,
         help='Image height [%(default)s]')
+    parser.add_argument('--egt', action='store_true',
+        help='Render in EGT mode')
     global args
     args = parser.parse_args()
 
@@ -211,8 +270,11 @@ def main():
     preProcess()
     cpus = cpu_count()
     log(f'Using {cpus} CPUs')
+
+    handler = processEGT if args.egt else process
+
     with Pool(cpus) as p:
-        p.map(process, queue)
+        p.map(handler, queue)
 
     print('Create the mp4 sequence with:')
     print(f"ffmpeg -r 1 -f image2 -i '%06d.png' -s {args.width}x{args.height} -pix_fmt yuv420p -r 29.97 g1000.mp4")
