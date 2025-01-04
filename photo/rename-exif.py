b29e7db89fc460eea28778a65dbce15c242986b3
Marlon Dutra <mfdutra@gmail.com>
2025-01-04 08:58:04 -0800

    rename-exif.py

---
+++ photo/rename-exif.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# Set filesystem modification time according to EXIF's time of creation
+# Rename the file to represent its mtime
+
+# Copyright 2011 Marlon Dutra
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import os
+import time
+
+for file in sys.argv[1:]:
+    # Extract EXIF info
+    if os.path.isfile(file) and os.access(file, os.R_OK):
+        exif = os.popen("exif -i " + file).readlines()
+
+        datetime = ""
+        # Look for capture date (tag 0x9003)
+        for line in exif:
+            if line.find("0x9003|") == 0:
+                datetime = line.split("|")[1].strip()
+
+        if not datetime:
+            print("Unable to read capture date from " + file)
+            continue
+
+        # Set mtime
+        mtime = time.strptime(datetime, "%Y:%m:%d %H:%M:%S")
+        os.utime(file, (-1, int(time.mktime(mtime))))
+
+        # Rename file
+        if "." in file:
+            ext = "." + file.rpartition(".")[2]
+        else:
+            ext = ""
+
+        filename = datetime.replace(":", "").replace(" ", "") + ext
+        os.rename(file, filename)
+
+        print(file + ": " + datetime)
